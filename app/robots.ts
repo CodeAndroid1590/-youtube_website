cdc028d6abe5838820f5db4696cb960fadbbc87a
CodeAndroid1590 <tiktoc8@gmail.com>
2026-09-02 01:23:15 +0500

    base url changed

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://youtube-website-zy1c.vercel.app";
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://devnesthub.com/";
 
   return {
     rules: {
